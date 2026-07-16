0bdb4a787419cd1a565dce93ce0af703e64b1d3b
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-07-16 21:37:04 +0545

    Added localStorage

--- index.js
+++ index.js
@@ -8,34 +8,28 @@ const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
-//Song Titles
+// Song Titles
+const songs = ['music1', 'music2', 'music3']
 
-const songs=['music1','music2','music3']
+// Keep Track of Song
+let songIndex = 1
 
-//Keep Track of Song
-
-let songIndex=1
-
-//Intially Load Song Into DOM
-
-loadSong(songs[songIndex])
-
-//Update the song details
-
-function loadSong(song){
+// Update the song details
+function loadSong(song) {
   title.innerText = song
-  audio.src= `soundtracks/${song}.mp3`
-  cover.src=`images/${song}.jpg`
+  audio.src = `soundtracks/${song}.mp3`
+  cover.src = `images/${song}.jpg`
 }
 
-function playSong(){
+function playSong() {
   musicContainer.classList.add('play')
   playBtn.querySelector('i.fas').classList.remove('fa-play')
   playBtn.querySelector('i.fas').classList.add('fa-pause')
 
   audio.play()
 }
-function pauseSong(){
+
+function pauseSong() {
   musicContainer.classList.remove('play')
   playBtn.querySelector('i.fas').classList.add('fa-play')
   playBtn.querySelector('i.fas').classList.remove('fa-pause')
@@ -43,66 +37,98 @@ function pauseSong(){
   audio.pause()
 }
 
-function prevSong(){
+function prevSong() {
   songIndex--
 
-  if (songIndex<0){
-    songIndex=songs.length-1
+  if (songIndex < 0) {
+    songIndex = songs.length - 1
   }
   loadSong(songs[songIndex])
   playSong()
 }
 
-function nextSong(){
+function nextSong() {
   songIndex++
 
-  if(songIndex>songs.length-1){
-    songIndex=0;
-
+  if (songIndex > songs.length - 1) {
+    songIndex = 0
   }
   loadSong(songs[songIndex])
   playSong()
 }
 
-function updateProgress(e){
-  const {duration, currentTime}= e.srcElement
-  const progressPercent = (currentTime/duration)*100
-  progress.style.width=`${progressPercent}%`
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement
+  if (duration) {
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+    saveToStorage()
+  }
 }
 
-function setProgress(e){
+function setProgress(e) {
   const width = this.clientWidth
   const clickX = e.offsetX
   const duration = audio.duration
 
-  audio.currentTime = (clickX/width)*duration
+  audio.currentTime = (clickX / width) * duration
 }
 
+function saveToStorage() {
+  localStorage.setItem('currentSongIndex', songIndex)
+  localStorage.setItem('currentTime', audio.currentTime)
+}
 
-//Event Listeners
+function loadFromStorage() {
+  const savedIndex = localStorage.getItem('currentSongIndex')
+  const savedTime = localStorage.getItem('currentTime')
 
-playBtn.addEventListener('click',()=>{
+  // 1. Fallback to default index if storage is empty
+  if (savedIndex !== null && songs[savedIndex]) {
+    songIndex = parseInt(savedIndex, 10)
+  }
+
+  // Load song into DOM
+  loadSong(songs[songIndex])
+
+  // 2. Restore timestamp and update progress bar width immediately
+  if (savedTime !== null) {
+    const restoreTimeAndBar = () => {
+      const currentTime = parseFloat(savedTime)
+      audio.currentTime = currentTime
+
+      // Calculate and set the bar width right away on load
+      if (audio.duration) {
+        const progressPercent = (currentTime / audio.duration) * 100
+        progress.style.width = `${progressPercent}%`
+      }
+    }
+
+    if (audio.readyState >= 1) {
+      restoreTimeAndBar()
+    } else {
+      audio.addEventListener('loadedmetadata', restoreTimeAndBar, { once: true })
+    }
+  }
+}
+
+// Initial storage load
+loadFromStorage()
+
+// Event Listeners
+playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play')
 
-  if (isPlaying){
+  if (isPlaying) {
     pauseSong()
-  }
-  else{
+  } else {
     playSong()
   }
-
 })
 
-prevBtn.addEventListener('click',()=>{
-  prevSong()
-})
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
 
-nextBtn.addEventListener('click',()=>{
-  nextSong()
-})
-
-audio.addEventListener('timeupdate',updateProgress)
-
-progressContainer.addEventListener('click',setProgress)
-
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
 audio.addEventListener('ended', nextSong)
