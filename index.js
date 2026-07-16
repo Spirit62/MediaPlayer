0e3a34c703418e8fbc7f07421a9e2265f15cf889
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-07-16 21:29:53 +0545

    Finished project

--- index.js
+++ index.js
@@ -63,6 +63,22 @@ function nextSong(){
   loadSong(songs[songIndex])
   playSong()
 }
+
+function updateProgress(e){
+  const {duration, currentTime}= e.srcElement
+  const progressPercent = (currentTime/duration)*100
+  progress.style.width=`${progressPercent}%`
+}
+
+function setProgress(e){
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX/width)*duration
+}
+
+
 //Event Listeners
 
 playBtn.addEventListener('click',()=>{
@@ -84,3 +100,9 @@ prevBtn.addEventListener('click',()=>{
 nextBtn.addEventListener('click',()=>{
   nextSong()
 })
+
+audio.addEventListener('timeupdate',updateProgress)
+
+progressContainer.addEventListener('click',setProgress)
+
+audio.addEventListener('ended', nextSong)
